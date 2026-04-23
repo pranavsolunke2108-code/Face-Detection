119110812d1a7d4e6a0f58d736622da9191918b4
pranav solunke <pranavsolunke2108@gmail.com>
2026-04-23 21:08:30 +0530

    add webcam using opencv to capture video and display

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to access camera")
+        break
+
+    cv2.imshow("Webcam", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # press ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()

df2ffff4a44b36d8d7571691227bccba3d4d207f
pranav solunke <pranavsolunke2108@gmail.com>
2026-04-28 21:06:13 +0530

    add face recognision

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import cv2
+import face_recognition
+import os
 import time
 
-# Load the face detection model
-face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
+# Load known faces
+known_encodings = []
+known_names = []
+
+for file in os.listdir("known_faces"):
+    img = face_recognition.load_image_file(f"known_faces/{file}")
+    encodings = face_recognition.face_encodings(img)
+
+if len(encodings) > 0:
+    known_encodings.append(encodings[0])
+    known_names.append(file.split(".")[0])
+else:
+    print(f"No face found in {file}")
+    known_encodings.append(encoding)
+    known_names.append(file.split(".")[0])
 
 cap = cv2.VideoCapture(0)
-prev_time = 0
 
 while True:
     ret, frame = cap.read()
-
     if not ret:
-        print("Failed to access camera")
         break
-    
-    current_time = time.time()
-    fps = 1 / (current_time - prev_time) if prev_time != 0 else 0
-    prev_time = current_time
-    
-    # Convert to grayscale (important for detection)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    face_count = len(faces)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    cv2.putText(frame, f"FPS: {int(fps)}", (10, 70),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    faces = face_recognition.face_locations(rgb)
+    encodings = face_recognition.face_encodings(rgb, faces)
 
-    # Draw rectangle around faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for (top, right, bottom, left), face_encoding in zip(faces, encodings):
 
-    cv2.imshow("Face Detection", frame)
+        matches = face_recognition.compare_faces(known_encodings, face_encoding)
+        name = "Unknown"
 
-    cv2.putText(frame, f"Faces B: {face_count}", (10, 30),
+        if True in matches:
+            index = matches.index(True)
+            name = known_names[index]
 
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    
-    
+        cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
+        cv2.putText(frame, name, (left, top-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+
+    cv2.imshow("Recognition", frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
